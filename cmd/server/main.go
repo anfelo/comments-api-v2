907c9aefcd9f59da400f611f228f0158b97cb739
Andres Osorio <af.osorio1341@gmail.com>
2022-03-18 15:09:40 -0500

    Added database package, db connection and ping

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/anfelo/comments-api-v2/internal/db"
+)
 
 // Run - Responsible for the instantiation
 // and startup of our go application
 func Run() error {
 	fmt.Println("Starting up our application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/anfelo/comments-api-v2
 
 go 1.17
+
+require (
+	github.com/jmoiron/sqlx v1.3.4 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+)
