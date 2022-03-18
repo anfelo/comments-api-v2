333ca1872753551169abfcc0905f0ca65a37a4d3
Andres Osorio <af.osorio1341@gmail.com>
2022-03-18 15:39:33 -0500

    Added database migrations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/anfelo/comments-api-v2/internal/db"
@@ -17,7 +16,8 @@ func Run() error {
 		fmt.Println("Failed to connect to the database")
 		return err
 	}
-	if err := db.Ping(context.Background()); err != nil {
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("failed to migrate database")
 		return err
 	}
 	return nil
