33319c92bbe7db1e8e489a25e6d0d5bf3b9c0541
Andres Osorio <af.osorio1341@gmail.com>
2022-03-18 06:55:29 -0500

    Added the rest of the methods for the service but not implemented yet

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -2,7 +2,13 @@ package comment
 
 import (
 	"context"
+	"errors"
 	"fmt"
+)
+
+var (
+	ErrFetchingComment = errors.New("failed to fetch comment by id")
+	ErrNotImplemented  = errors.New("not Implemented")
 )
 
 // Comment - a representation of the commnet
@@ -15,9 +21,12 @@ type Comment struct {
 }
 
 // Store - the main interface that describes how to
-// interact with the store
+// interact with the store or repository layer
 type Store interface {
 	GetComment(context.Context, string) (Comment, error)
+	UpdateComment(ctx context.Context, cmt Comment) error
+	DeleteComment(ctx context.Context, id string) error
+	CreateComment(ctx context.Context, cmt Comment) (Comment, error)
 }
 
 // Service - is the struct on which all our logic
@@ -33,12 +42,27 @@ func NewService(store Store) *Service {
 	}
 }
 
-// GetComment - return a comment by its ID
+// GetComment - returns a comment by its ID
 func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
 	cmt, err := s.Store.GetComment(ctx, id)
 	if err != nil {
 		fmt.Println(err)
-		return Comment{}, err
+		return Comment{}, ErrFetchingComment
 	}
 	return cmt, nil
+}
+
+// UpdateComment - updates a comment
+func (s *Service) UpdateComment(ctx context.Context, cmt Comment) error {
+	return ErrNotImplemented
+}
+
+// DeleteComment - deletes a comment by its id
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+// CreateComment - creates a new comment
+func (s *Service) CreateComment(ctx context.Context, cmt Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
 }
